a3233e0148ac5be87aeeeca3224024c84b81486d
dreysmal <sdbyss@gmail.com>
2018-05-05 21:57:41 +0300

    2 commit

---
+++ File_System_2/File_manage.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace File_System_2
+{
+    class File_manage
+    {
+        static int i;
+        public static Dictionary<int, string> Coincidences = new Dictionary<int, string>();
+        public Regex regMask;
+        public void GetMask()
+        {
+            WriteLine("Enter name of file or directory for searching or mask:");
+            string mask = ReadLine();
+            Clear();
+            mask = mask.Replace(".", "\\.");
+            mask = mask.Replace("?", ".");
+            mask = mask.Replace("*", ".*");
+            mask = "^" + mask + "$";
+            regMask = new Regex(mask, RegexOptions.IgnoreCase);
+        }
+        public void Search()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                try
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(drive.ToString());  //drive.ToString()
+            Searching(directoryInfo, regMask);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+        static void Searching(DirectoryInfo dir_or_file_name, Regex mask)
+        {
+            FileInfo[] fi = dir_or_file_name.GetFiles();
+            foreach (FileInfo item in fi)
+            {
+                if (mask.IsMatch(item.Name))
+                {
+                    WriteLine((i + 1) + " File: " + item.FullName);
+                    Coincidences.Add(i++, item.FullName);
+                }
+            }
+            DirectoryInfo[] subdir = dir_or_file_name.GetDirectories();
+            foreach (DirectoryInfo item in subdir)
+            {
+                try
+                {
+                    if (mask.IsMatch(item.Name))
+                    {
+                        WriteLine((i + 1) + " Dir: " + item.FullName);
+                        Coincidences.Add(i++, item.FullName);
+                    }
+                    Searching(item, mask);
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine("Access denied to " + item.ToString());
+                    continue;
+                }
+            }
+        }
+        public void DeleteAll()
+        {
+            foreach (String item in Coincidences.Values)
+            {
+                FileInfo fileInf = new FileInfo(item);
+                if (fileInf.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    try
+                    {
+                        Directory.Delete(item, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        File.Delete(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.Message);
+                        continue;
+                    }
+                }
+            }
+        }
+        public void DeleteSpecific(int x)
+        {
+            FileInfo fileInf = new FileInfo(Coincidences[x - 1]);
+            if (fileInf.Attributes.HasFlag(FileAttributes.Directory))
+            {
+                try
+                {
+                    Directory.Delete(Coincidences[x - 1], true);
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.Delete(Coincidences[x - 1]);
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+        }
+        public void DeleteRange(int x1, int x2)
+        {
+            for (int i = x1-1; i < x2-1; i++)
+            {
+                FileInfo fileInf = new FileInfo(Coincidences[i]);
+                if (fileInf.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    try
+                    {
+                        Directory.Delete(Coincidences[i], true);
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        File.Delete(Coincidences[i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ File_System_2/Program.cs
@@ -0,0 +1,115 @@
+﻿//2. Написать приложение для поиска по всему диску файлов и
+//каталогов, удовлетворяющих заданной маске. Необходимо вывести
+//найденную информацию на экран в компактном виде (с нумерацией
+//объектов) и запросить у пользователя о дальнейших действиях.
+//Варианты действий: удалить все найденное, удалить указанный файл
+//(каталог), удалить диапазон файлов(каталогов). 
+using static System.Console;
+using System;
+
+namespace File_System_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            File_manage F_manager = new File_manage();
+            F_manager.GetMask();
+            F_manager.Search();
+            if(File_manage.Coincidences.Count>0)
+            {
+                WriteLine("\n" + @"Press:
+1 - Delete all found files and directories.
+2 - Enter menu of deleting chosen file or directory.
+3 - Enter menu of entering range of numbers of files and directories to delete.");
+                bool flag = true;
+                while (flag)
+                {
+                    ConsoleKeyInfo keypress = ReadKey();
+                    switch (keypress.Key)
+                    {
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:
+                            flag = false;
+                            F_manager.DeleteAll();
+                            WriteLine("All files deleted");
+                            break;
+                        case ConsoleKey.D2:
+                        case ConsoleKey.NumPad2:
+                            flag = false;
+                            WriteLine("\nEnter number of file or directory to delete.");
+                            int x = 0;
+                            while (true)
+                            {
+                                try
+                                {
+                                    x = Int32.Parse(ReadLine());
+                                    break;
+                                }
+                                catch (FormatException ex)
+                                {
+                                    WriteLine(ex.Message + "try again");
+                                    continue;
+                                }
+                                catch (ArgumentNullException ex1)
+                                {
+                                    WriteLine(ex1.Message + "try again");
+                                    continue;
+                                }
+                            }
+                            F_manager.DeleteSpecific(x);
+                            break;
+                        case ConsoleKey.D3:
+                        case ConsoleKey.NumPad3:
+                            flag = false;
+                            WriteLine("\nSet Range of files or directories to delete.\nEnter first number:");
+                            int x1 = 0;
+                            int x2 = 0;
+                            while (true)
+                            {
+                                try
+                                {
+                                    x1 = Int32.Parse(ReadLine());
+                                    break;
+                                }
+                                catch (FormatException ex)
+                                {
+                                    WriteLine(ex.Message + "try again");
+                                    continue;
+                                }
+                                catch (ArgumentNullException ex1)
+                                {
+                                    WriteLine(ex1.Message + "try again");
+                                    continue;
+                                }
+                            }
+                            WriteLine("Enter second number:");
+                            while (true)
+                            {
+                                try
+                                {
+                                    x2 = Int32.Parse(ReadLine());
+                                    break;
+                                }
+                                catch (FormatException ex)
+                                {
+                                    WriteLine(ex.Message + "try again");
+                                    continue;
+                                }
+                                catch (ArgumentNullException ex1)
+                                {
+                                    WriteLine(ex1.Message + "try again");
+                                    continue;
+                                }
+                            }
+                            F_manager.DeleteRange(x1, x2);
+                            break;
+                        default:
+                            flag = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
